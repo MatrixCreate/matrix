b3f1f60eaa748a1727bfadb37e292dd529d4d0d7
Adam Glaysher <adamglaysher@gmail.com>
2022-10-28 18:16:30 +0100

    init

---
+++ go.mod
@@ -0,0 +1,18 @@
+module matrix
+
+go 1.19
+
+require github.com/urfave/cli/v2 v2.20.3
+
+require (
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
+
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/fatih/color v1.13.0
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
---
+++ matrix.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+)
+
+var craftStarterRepo = "git@github.com:MatrixCreate/craft-starter.git"
+var projectName = ""
+
+func main() {
+	app := &cli.App{
+		Name:      "Matrix CLI",
+		Version:   "v1.0",
+		Copyright: "(c) 2022 Matrix Create",
+		Usage:     "Project Management CLI Tool",
+		Commands: []*cli.Command{
+			{
+				Name:    "create",
+				Aliases: []string{"c"},
+				Usage:   "Create a new Craft CMS project",
+				Action: func(cCtx *cli.Context) error {
+					projectName = cCtx.Args().First()
+
+					if projectName == "" {
+						color.Red("Missing project name")
+						return nil
+					}
+
+					color.Magenta("Creating new Craft CMS project: " + projectName)
+
+					// git clone --depth=1 {craftStarterRepo} {projectName}
+					cmd := exec.Command("git", "clone", "--depth=1", craftStarterRepo, projectName)
+					cmdErr := cmd.Run()
+					if cmdErr != nil {
+						if cmdErr.Error() == "exit status 128" {
+							color.Red("Project already exists")
+							return nil
+						}
+
+						color.Red("Error (git clone): " + cmdErr.Error())
+						return nil
+					}
+
+					color.Green("✓ git clone craft-starter")
+
+					// ddev config --project-name=
+					runCommand(exec.Command("ddev", "config", "--project-name="+projectName), false)
+					color.Green("✓ ddev config --project-name=" + projectName)
+
+					// composer install
+					runCommand(exec.Command("composer", "install"), false)
+					color.Green("✓ composer install")
+
+					// npm install
+					runCommand(exec.Command("npm", "install"), false)
+					color.Green("✓ npm install")
+
+					// ddev start
+					runCommand(exec.Command("ddev", "start"), false)
+					color.Green("✓ ddev start")
+
+					// ddev craft setup/app-id
+					runCommand(exec.Command("ddev", "craft", "setup/app-id"), false)
+					color.Green("✓ ddev craft setup/app-id")
+
+					// ddev craft setup/security-key
+					runCommand(exec.Command("ddev", "craft", "setup/security-key"), false)
+					color.Green("✓ ddev craft setup/security-key")
+
+					// ddev craft setup/db --interactive=0 --driver=mysql --database=db --password=db --user=db --server=ddev-{projectName}-db --port=3306
+					runCommand(exec.Command("ddev", "craft", "setup/db", "--interactive=0", "--driver=mysql", "--database=db", "--password=db", "--user=db", "--server=ddev-"+projectName+"-db", "--port=3306"), false)
+					color.Green("✓ ddev craft setup/db")
+
+					// ddev import-db --src=_db/db.zip
+					runCommand(exec.Command("ddev", "import-db", "--src=_db/db.zip"), false)
+					color.Green("✓ ddev import-db")
+
+					// rm -rf ./dirformynewrepo/.git
+					runCommand(exec.Command("rm", "-rf", "./"+projectName+"/.git"), false)
+					color.Green("✓ rm -rf ./" + projectName + "/.git")
+
+					// git init
+					runCommand(exec.Command("git", "init"), false)
+					color.Green("✓ git init")
+
+					// ddev describe
+					runCommand(exec.Command("ddev", "describe"), true)
+
+					color.Magenta("Project Ready!")
+
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runCommand(cmd *exec.Cmd, showOutput bool) {
+	cmd.Dir = "./" + projectName
+
+	if showOutput {
+		out, err := cmd.Output()
+		if err != nil {
+			color.Red("Error: " + err.Error())
+		}
+		fmt.Println(string(out))
+	} else {
+		err := cmd.Run()
+		if err != nil {
+			color.Red("Error: " + err.Error())
+		}
+	}
+}
