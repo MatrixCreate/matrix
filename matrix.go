d9e95561829f52322d21ad4aa53d009a34df0cee
Adam Glaysher <adamglaysher@gmail.com>
2022-11-15 16:42:48 +0000

    Improve errors

--- matrix.go
+++ matrix.go
@@ -16,13 +16,14 @@ import (
 
 var craftStarterRepo string = "git@github.com:MatrixCreate/craft-starter.git"
 var projectName string = ""
+var commandCount int = 0
 
 func main() {
 	setupEnv()
 
 	app := &cli.App{
 		Name:      "Matrix CLI",
-		Version:   "v1.0.6",
+		Version:   "v1.0.8",
 		Copyright: "(c) 2022 Matrix Create",
 		Usage:     "Project Management CLI Tool",
 		Commands: []*cli.Command{
@@ -34,20 +35,20 @@ func main() {
 					projectName = cCtx.Args().First()
 
 					if projectName == "" {
-						color.Red("Missing project name")
-						return nil
+						color.Red("× Error: Missing project name")
+						os.Exit(1)
 					}
 
 					if fileExists(projectName) {
-						color.Red("Project directory already exists")
-						os.Exit(0)
+						color.Red("× Error: Project directory already exists")
+						os.Exit(2)
 					}
 
 					color.Magenta("Creating new Craft CMS project: " + projectName)
 
-					setupCraftCMS(true)
+					setupProject(true)
 
-					color.Magenta("Project Ready! /" + projectName)
+					color.Magenta("Project Ready! cd " + projectName)
 
 					return nil
 				},
@@ -72,15 +73,20 @@ func main() {
 					projectName = cCtx.Args().First()
 
 					if projectName == "" {
-						color.Red("Missing project name")
-						return nil
+						color.Red("× Error: Missing project name")
+						os.Exit(1)
 					}
 
-					color.Magenta("Setting up existing Craft CMS project to edit: " + projectName)
+					if fileExists(projectName) {
+						color.Red("× Error: Project directory already exists")
+						os.Exit(2)
+					}
 
-					setupCraftCMS(false)
+					color.Magenta("Setting up existing project to edit: " + projectName)
 
-					color.Magenta("Project Ready! /" + projectName)
+					setupProject(false)
+
+					color.Magenta("Project Ready! cd " + projectName)
 
 					return nil
 				},
@@ -102,76 +108,76 @@ func setupEnv() {
 	}
 }
 
-func setupCraftCMS(fresh bool) {
+func setupProject(fresh bool) {
 	if fresh {
 		// git clone --depth=1 {craftStarterRepo} {projectName}
-		runCommand(exec.Command("git", "clone", "--depth=1", craftStarterRepo, projectName), false, false)
-		color.Green("✓ git clone " + craftStarterRepo)
+		runCommand(exec.Command("git", "clone", "--depth=1", craftStarterRepo, projectName), false, false, true)
+		color.Green("✓ git clone --depth=1 " + craftStarterRepo + " " + projectName)
 
 		// ddev config --project-name={projectName}
-		runCommand(exec.Command("ddev", "config", "--project-name="+projectName), false, true)
+		runCommand(exec.Command("ddev", "config", "--project-name="+projectName), false, true, false)
 		color.Green("✓ ddev config --project-name=" + projectName)
 	} else {
 		// git clone -b develop git@bitbucket.org:matrixcreate/{projectName}.git {projectName}
-		runCommand(exec.Command("git", "clone", "-b", "develop", "git@bitbucket.org:matrixcreate/"+projectName+".git", projectName), false, false)
-		color.Green("✓ git clone git@bitbucket.org:matrixcreate/" + projectName + ".git")
+		runCommand(exec.Command("git", "clone", "-b", "develop", "git@bitbucket.org:matrixcreate/"+projectName+".git", projectName), false, false, true)
+		color.Green("✓ git clone -b develop git@bitbucket.org:matrixcreate/" + projectName + ".git" + " " + projectName)
 	}
 
 	// ddev start
-	runCommand(exec.Command("ddev", "start"), false, true)
+	runCommand(exec.Command("ddev", "start"), false, true, true)
 	color.Green("✓ ddev start")
 
 	// ddev composer install
-	if fileExists("./" + projectName + "/composer.lock") {
-		runCommand(exec.Command("ddev", "composer", "install"), false, true)
+	if fileExists(projectName + "/composer.lock") {
+		runCommand(exec.Command("ddev", "composer", "install"), false, true, false)
 		color.Green("✓ ddev composer install")
 	} else {
-		color.Green("- No composer.lock file found. Skipping composer install")
+		color.Yellow("- No composer.lock file found. Skipping composer install")
 	}
 
 	// ddev npm install
-	if fileExists("./" + projectName + "/package-lock.json") {
-		runCommand(exec.Command("ddev", "npm", "install"), false, true)
+	if fileExists(projectName + "/package-lock.json") {
+		runCommand(exec.Command("ddev", "npm", "install"), false, true, false)
 		color.Green("✓ ddev npm install")
 	} else {
-		color.Green("- No package-lock.json file found. Skipping npm install")
+		color.Yellow("- No package-lock.json file found. Skipping npm install")
 	}
 
 	// ddev craft setup/app-id --interactive=0
-	runCommand(exec.Command("ddev", "craft", "setup/app-id", "--interactive=0"), false, true)
+	runCommand(exec.Command("ddev", "craft", "setup/app-id", "--interactive=0"), false, true, false)
 	color.Green("✓ ddev craft setup/app-id")
 
 	// ddev craft setup/security-key
-	runCommand(exec.Command("ddev", "craft", "setup/security-key"), false, true)
+	runCommand(exec.Command("ddev", "craft", "setup/security-key"), false, true, false)
 	color.Green("✓ ddev craft setup/security-key")
 
 	// ddev craft setup/db --interactive=0 --driver=mysql --database=db --password=db --user=db --server=ddev-{projectName}-db --port=3306
-	runCommand(exec.Command("ddev", "craft", "setup/db", "--interactive=0", "--driver=mysql", "--database=db", "--password=db", "--user=db", "--server=ddev-"+projectName+"-db", "--port=3306"), false, true)
-	color.Green("✓ ddev craft setup/db")
+	runCommand(exec.Command("ddev", "craft", "setup/db", "--interactive=0", "--driver=mysql", "--database=db", "--password=db", "--user=db", "--server=ddev-"+projectName+"-db", "--port=3306"), false, true, false)
+	color.Green("✓ ddev craft setup/db --driver=mysql --database=db --password=db --user=db --server=ddev-" + projectName + "-db --port=3306")
 
 	// ddev import-db --src=_db/db.zip
 	if fileExists("./" + projectName + "/_db/db.zip") {
-		runCommand(exec.Command("ddev", "import-db", "--src=_db/db.zip"), false, true)
-		color.Green("✓ ddev import-db")
+		runCommand(exec.Command("ddev", "import-db", "--src=_db/db.zip"), false, true, false)
+		color.Green("✓ ddev import-db --src=_db/db.zip")
 	} else {
-		color.Green("- No _db/db.zip file found. Skipping ddev import-db")
+		color.Yellow("- No _db/db.zip file found. Skipping ddev import-db")
 	}
 
 	if fresh {
 		// rm -rf ./{projectName}/.git
-		runCommand(exec.Command("rm", "-rf", "./"+projectName+"/.git"), false, true)
+		runCommand(exec.Command("rm", "-rf", "./"+projectName+"/.git"), false, true, false)
 		color.Green("✓ rm -rf ./" + projectName + "/.git")
 
 		// git init
-		runCommand(exec.Command("git", "init"), false, true)
+		runCommand(exec.Command("git", "init"), false, true, false)
 		color.Green("✓ git init")
 	}
 
 	// ddev describe
-	runCommand(exec.Command("ddev", "describe"), true, true)
+	runCommand(exec.Command("ddev", "describe"), true, true, false)
 }
 
-func runCommand(cmd *exec.Cmd, showOutput bool, inProject bool) {
+func runCommand(cmd *exec.Cmd, showOutput bool, inProject bool, exitOnError bool) {
 	if inProject {
 		cmd.Dir = "./" + projectName
 	}
@@ -179,33 +185,48 @@ func runCommand(cmd *exec.Cmd, showOutput bool, inProject bool) {
 	if showOutput {
 		out, err := cmd.Output()
 		if err != nil {
-			color.Red("Error Running: " + cmd.String())
-			color.Red("Error: " + err.Error())
-			os.Exit(0)
+			if exitOnError {
+				color.Red("× Error Running: " + cmd.String())
+				color.Red(err.Error())
+				os.Exit(commandCount)
+			} else {
+				color.Yellow("× Error Running: " + cmd.String())
+				color.Yellow(err.Error())
+			}
 		}
 		fmt.Println(string(out))
 	} else {
 		err := cmd.Run()
 		if err != nil {
-			color.Red("Error Running: " + cmd.String())
-			color.Red("Error: " + err.Error())
-			os.Exit(0)
+			if exitOnError {
+				color.Red("× Error Running: " + cmd.String())
+				color.Red(err.Error())
+				os.Exit(commandCount)
+			} else {
+				color.Yellow("× Error Running: " + cmd.String())
+				color.Yellow(err.Error())
+			}
 		}
 	}
+
+	commandCount++
 }
 
 func runRemoteCommand(command string) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		color.Red("Error: Unable to find home dir: %v", err)
+		color.Red("× Error: Unable to find home dir: %v", err)
+		os.Exit(1)
 	}
 	key, err := os.ReadFile(homeDir + "/.ssh/id_rsa")
 	if err != nil {
-		color.Red("Error: Unable to read private key: %v", err)
+		color.Red("× Error: Unable to read private key: %v", err)
+		os.Exit(2)
 	}
 	signer, err := ssh.ParsePrivateKey(key)
 	if err != nil {
-		color.Red("Error: Unable to parse private key: %v", err)
+		color.Red("× Error: Unable to parse private key: %v", err)
+		os.Exit(3)
 	}
 	hostKeyCallback, err := knownhosts.New(homeDir + "/.ssh/known_hosts")
 	if err != nil {
@@ -221,20 +242,20 @@ func runRemoteCommand(command string) {
 	}
 	client, err := ssh.Dial("tcp", os.Getenv("REMOTE_SERVER_IP")+":22", config)
 	if err != nil {
-		log.Fatal("Failed to dial: ", err)
+		log.Fatal("× Failed to dial: ", err)
 	}
 	defer client.Close()
 
 	session, err := client.NewSession()
 	if err != nil {
-		log.Fatal("Failed to create session: ", err)
+		log.Fatal("× Failed to create session: ", err)
 	}
 	defer session.Close()
 
 	var b bytes.Buffer
 	session.Stdout = &b
 	if err := session.Run(command); err != nil {
-		log.Fatal("Failed to run: " + err.Error())
+		log.Fatal("× Failed to run: " + err.Error())
 	}
 
 	color.White(b.String())
